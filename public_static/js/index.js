ba0d4791570eab54abc5aef416692907b7f00d12
Bhavya Aggarwal <bhavyaaggarwal1209@gmail.com>
2017-12-29 18:50:02 +0530

    Update HTML
    Signed-off-by: Bhavya Aggarwal <bhavyaaggarwal1209@gmail.com>

--- public_static/js/index.js
+++ public_static/js/index.js
@@ -37,7 +37,8 @@ $(document).ready(function () {
     $('#addPartyMaster').click(function () {
 
       $mainContent.empty();
-      $mainContent.append(`<div class="form-group row">
+      $mainContent.append(`
+<div class="form-group row">
   <label for="example-text-input" class="col-2 col-form-label">Text</label>
   <div class="col-10">
     <input class="form-control" type="text" value="Artisanal kale" id="example-text-input">
