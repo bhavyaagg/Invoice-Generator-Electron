1baaedc0f3e9b1548916cfd8d11086618e3377d3
Bhavya Aggarwal <bhavyaaggarwal1209@gmail.com>
2017-12-29 16:22:15 +0530

    Setup bootstrap and jquery
    Signed-off-by: Bhavya Aggarwal <bhavyaaggarwal1209@gmail.com>

--- main.js
+++ main.js
@@ -3,17 +3,38 @@
  */
 
 const {app, BrowserWindow} = require('electron');
-const path = require('path')
+const path = require('path');
 const url = require('url');
 
-let win;
-app.on('ready', () => {
-  win = new BrowserWindow({width: 200, height: 200});
+let mainWindow;
 
-  win.loadURL(url.format({
-    pathname: path.join(__dirname, 'index.html'),
+function createWindow() {
+  mainWindow = new BrowserWindow({width: 400, height: 400});
+  console.log(process.versions.node)
+  mainWindow.loadURL(url.format({
+    pathname: path.join(__dirname, 'public_static', 'index.html'),
     protocol: 'file:',
     slashes: true
   }))
 
-})
+  mainWindow.webContents.openDevTools();
+
+  mainWindow.on('closed', () => {
+    mainWindow = null;
+    console.log("Window Closed")
+  })
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+});
+
+app.on('activate', () => {
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
---
+++ public_static/js/index.js
@@ -0,0 +1,8 @@
+/**
+ * Created by bhavyaagg on 29/12/17.
+ */
+
+$(document).ready(function () {
+  console.log(`We are using node ${process.versions.node}, 
+               Chrome ${process.versions.chrome}, and Electron ${process.versions.electron}.`)
+});
