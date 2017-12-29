01abff46082b59a3e45e8f28e1a8e7297c100c28
Bhavya Aggarwal <bhavyaaggarwal1209@gmail.com>
2017-12-29 22:30:57 +0530

    Add Basic DB
    Signed-off-by: Bhavya Aggarwal <bhavyaaggarwal1209@gmail.com>

---
+++ db/models.js
@@ -0,0 +1,35 @@
+/**
+ * Created by bhavyaagg on 29/12/17.
+ */
+
+let config;
+
+try {
+  config = require('./../secrets');
+} catch (e) {
+  config = require('./../secrets-sample');
+  console.log("Create your own secrets");
+}
+
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize(config.db.NAME, config.db.USER, config.db.PASSWORD, {
+  host: config.db.HOST,
+  dialect: config.db.DIALECT,
+  pool: {
+    max: 5,
+    min: 0
+  },
+  storage: config.db.STORAGE
+});
+
+const ProductCategory = sequelize.define('productcategory', {
+  name: Sequelize.DataTypes.STRING
+});
+
+sequelize.sync({force: false}).then(function () {
+  console.log("Database Configured");
+});
+
+module.exports = {
+  ProductCategory
+}
