5948d1e8456e7690cb29f00868082a2a7b0558f4
Bhavya Aggarwal <bhavyaaggarwal1209@gmail.com>
2018-01-02 21:08:30 +0530

    Modular code into routes. Add ProductCategoryRoute
    Signed-off-by: Bhavya Aggarwal <bhavyaaggarwal1209@gmail.com>

---
+++ routes/ProductCategory.js
@@ -0,0 +1,93 @@
+/**
+ * Created by bhavyaagg on 02/01/18.
+ */
+
+const models = require('./../db/models');
+
+function addProductCategory(event, data) {
+  models.ProductCategory.create({
+    name: data.productCategoryName
+  }).then(function (result) {
+    event.sender.send('addedProductCategory', {
+      success: true
+    });
+  }).catch(function (err) {
+    event.sender.send('addedProductCategory', {
+      success: false,
+      error: err
+    })
+  })
+}
+
+function viewProductCategories(event) {
+  models.ProductCategory.findAll({}).then(function (rows) {
+    event.sender.send('getProductCategories', {
+      success: true,
+      productCategories: rows.map((v) => v.get())
+    });
+  }).catch(function (err) {
+    event.sender.send('getProductCategories', {
+      success: false,
+      error: err
+    });
+  })
+}
+
+function viewProductCategoryById(event, productCategory) {
+  models.ProductCategory.find({
+    where: {
+      id: productCategory.id
+    }
+  }).then(function (productCategory) {
+    if (productCategory) {
+      event.sender.send('getProductCategoryById', {
+        success: true,
+        productCategory: productCategory.get()
+      })
+    } else {
+      event.sender.send('getProductCategoryById', {
+        success: false,
+        error: "Incorrect Product Category ID"
+      })
+    }
+  }).catch(function (err) {
+    event.sender.send('getProductCategoryById', {
+      success: false,
+      error: err
+    });
+  })
+}
+
+function editProductCategory(event, productCategory) {
+  models.ProductCategory.update({
+    name: productCategory.name
+  }, {
+    where: {
+      id: productCategory.id
+    }
+  }).then(function (result) {
+    if (result[0] > 0) {
+      event.sender.send('editedProductCategory', {
+        success: true,
+      })
+    } else {
+      event.sender.send('editedProductCategory', {
+        success: false,
+        error: "Incorrect ID"
+      })
+    }
+  }).catch(function (err) {
+    console.log(err)
+    event.sender.send('editedProductCategory', {
+      success: false,
+      error: err
+    });
+  })
+}
+
+module.exports = exports = {
+  addProductCategory,
+  viewProductCategories,
+  viewProductCategoryById,
+  editProductCategory
+};
---
+++ routes/index.js
@@ -0,0 +1,7 @@
+/**
+ * Created by bhavyaagg on 02/01/18.
+ */
+
+module.exports = exports = {
+  productCategory: require('./ProductCategory')
+};
