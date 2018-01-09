68d49166c959d14de0d519904a21aa562240c83c
nimit95 <nimitagg95@gmail.com>
2018-01-09 11:40:29 +0530

    view invoice details
    Signed-off-by: nimit95 <nimitagg95@gmail.com>

--- routes/InvoiceDetail.js
+++ routes/InvoiceDetail.js
@@ -1,7 +1,38 @@
-
 /**
  * Created by nimit95 on 09/01/18.
  */
 
 
 const models = require('./../db/models');
+
+function viewInvoiceDetailsByIds(event, data) {
+  models.InvoiceDetail.findAll({
+    where: {
+      invoiceId: data.invoiceId,
+      productcategoryId: data.productcategoryId
+    }
+  }).then(resultRows => {
+    if (resultRows.length > 0) {
+      event.sender.send('getInvoiceDetailByIds', {
+        success: true,
+        invoiceItems: resultRows.map(v=>{
+          v = v.get();
+          v.invoice = v.invoice.get();
+          v.productcategory = v.productcategory.get();
+          return v;
+        })
+      })
+    }
+    else{
+      event.sender.send('getInvoiceDetailByIds', {
+        success: false,
+        error: "No object Found"
+      })
+    }
+  }).catch(err => {
+    event.sender.send('getInvoiceDetailByIds', {
+      success: false,
+      error: err
+    })
+  })
+}
