cb8cc4178ec3eadffeda46aa3dfb0ddb0ecae96b
Bhavya Aggarwal <bhavyaaggarwal1209@gmail.com>
2019-01-04 19:57:27 +0530

    Sort products alphabetically
    Signed-off-by: Bhavya Aggarwal <bhavyaaggarwal1209@gmail.com>

--- public_static/js/index.js
+++ public_static/js/index.js
@@ -2716,6 +2716,8 @@ $(document).ready(function () {
               </li>
           `;
 
+          data.products.sort(function(a,b) {return a.name>b.name})
+
           data.products.forEach(function (product) {
             str += `
             <li class="list-group-item">
